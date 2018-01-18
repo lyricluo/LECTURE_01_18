66db39f394cccd7b352e31429aacb1f14de7b610
Phill Conrad <pconrad@cs.ucsb.edu>
2018-01-18 12:07:27 -0800

    First versions of code

---
+++ Rational.java
@@ -0,0 +1,17 @@
+public class Rational {
+
+    private int num;
+    private int denom;
+
+    public Rational(int num, int denom){
+	this.num = num;
+	this.denom = denom;
+    }
+
+    public static void main(String [] args) {
+	Rational r = new Rational(3,4);
+	System.out.println("r = " + r);
+	// System.out.println("r = " + r.toString());
+    }
+
+}
